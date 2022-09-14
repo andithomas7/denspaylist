7ac2c2d8de8512fe979605463ddffd36f6d7904e
andithomas7 <110560618+andithomas7@users.noreply.github.com>
2022-09-14 21:56:08 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,46 @@
+#EXTINF:-1 tvg-logo="" group-title="Local", SCTV
+https://denstv1.herokuapp.com/qwr9ew/s/s03/index.m3u8
+#EXTINF:-1 tvg-logo="" group-title="Local", TransTV 
+https://denstv1.herokuapp.com/qwr9ew/s/s05/index.m3u8
+#EXTINF:-1 tvg-logo="" group-title="Local", TransTV 
+https://denstv1.herokuapp.com/
+#EXTINF:-1 tvg-logo="" group-title="Local", NET 
+https://denstv1.herokuapp.com/qwr9ew/s/s08/index.m3u8
+#EXTINF:-1 tvg-logo="" group-title="Local", Elshinta TV 
+https://denstv1.herokuapp.com/qwr9ew/s/s10/index.m3u8
+#EXTINF:-1 tvg-logo="" group-title="Local", TVRI  
+https://denstv1.herokuapp.com/qwr9ew/s/s11/index.m3u8
+
+#EXTINF:-1 tvg-logo="" group-title="MOVIE", Celestial Classic Movies
+https://denstv1.herokuapp.com/qwr9ew/s/s31/index.m3u8
+#EXTINF:-1 tvg-logo="" group-title="MOVIE", Zee Bioskop 
+https://denstv1.herokuapp.com/qwr9ew/s/s32/index.m3u8
+#EXTINF:-1 tvg-logo="" group-title="MOVIE", THRILL TV 
+https://denstv1.herokuapp.com/qwr9ew/s/s34/index.m3u8
+#EXTINF:-1 tvg-logo="" group-title="MOVIE", K+
+https://denstv1.herokuapp.com/uq2663/h/h08/index.m3u8
+#EXTINF:-1 tvg-logo="" group-title="MOVIE", My Cinema Europe Asia 
+https://denstv1.herokuapp.com//uq2663/h/h18/index.m3u8
+#EXTINF:-1 tvg-logo="" group-title="MOVIE", TVN 
+https://denstv1.herokuapp.com/dr9445/h/h20/index.m3u8
+#EXTINF:-1 tvg-logo="" group-title="MOVIE", TVN Movies 
+https://denstv1.herokuapp.com/dr9445/h/h21/index.m3u8
+#EXTINF:-1 tvg-logo="" group-title="MOVIE", Hits  
+https://denstv1.herokuapp.com/dr9445/h/h37/index.m3u8
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
